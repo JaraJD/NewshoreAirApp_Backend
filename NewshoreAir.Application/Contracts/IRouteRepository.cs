34857ebb5b5a6d8aca26df1200e6a336ac941764
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-06-10 14:03:26 -0500

    Add Repository Route
    The implementation of the repository of the Route entity is carried out

---
+++ NewshoreAir.Application/Contracts/IRouteRepository.cs
@@ -0,0 +1,9 @@
+﻿using NewshoreAir.Application.Contracts.DataAccess;
+using NewshoreAir.Business.Entities;
+
+namespace NewshoreAir.Application.Contracts
+{
+	public interface IRouteRepository : IAsyncRepository<Route>
+	{
+	}
+}
---
+++ NewshoreAir.Business/Entities/Route.cs
@@ -0,0 +1,12 @@
+﻿
+using NewshoreAir.Business.Entities.Common;
+
+namespace NewshoreAir.Business.Entities
+{
+	public class Route : BaseDomainModel
+	{
+		public string? FlightCarrier { get; set; }
+
+		public string? FlightNumber { get; set; }
+	}
+}

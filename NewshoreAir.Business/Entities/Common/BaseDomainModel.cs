563e29473f274e662349b29a780bb58cb555a006
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-06-10 09:09:14 -0500

    Class modeling
    Modeling of the proposed entities is carried out

---
+++ NewshoreAir.Business/Entities/Common/BaseDomainModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewshoreAir.Business.Entities.Common
+{
+	public  class BaseDomainModel
+	{
+		public string? Origin { get; set; }
+
+		public string? Destination { get; set; }
+
+		public decimal Price { get; set; }
+	}
+}
---
+++ NewshoreAir.Business/Entities/Flight.cs
@@ -0,0 +1,9 @@
+﻿using NewshoreAir.Business.Entities.Common;
+
+namespace NewshoreAir.Business.Entities
+{
+	public class Flight : BaseDomainModel
+	{
+		public Transport? Transport { get; set; }
+	}
+}

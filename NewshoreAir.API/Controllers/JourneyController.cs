b4be0be0318dee0cf37b652adf5ba39ce54494c3
JuanJaramilloSalazar <salazarjdj@gmail.com>
2023-06-10 23:31:45 -0500

    Journey Controller
    Implementation of the controller of the journey class

---
+++ NewshoreAir.API/Controllers/JourneyController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NewshoreAir.Application.Features.Routes.Queries.GetJourney;
+using System.Net;
+
+namespace NewshoreAir.API.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class JourneyController : ControllerBase
+	{
+		private readonly IMediator _mediator;
+
+		public JourneyController(IMediator mediator)
+		{
+			_mediator = mediator;
+		}
+
+		[HttpGet("Journey/{origin}/{destination}", Name = "GetJourney")]
+		[ProducesResponseType(typeof(JourneyVm), (int)HttpStatusCode.OK)]
+		public async Task<ActionResult<JourneyVm>> GetTargetSavingBySUser(string origin, string destination)
+		{
+			var query = new GetJourneyQuery(origin, destination);
+			var journey = await _mediator.Send(query);
+			return Ok(journey);
+		}
+	}
+}
